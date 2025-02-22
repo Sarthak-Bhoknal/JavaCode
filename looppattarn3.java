fb867025b778039aba57cf9e3fab8cc6d90f8e96
Sarthak-Bhoknal <sarthakbhoknal05@gmail.com>
2025-02-22 08:57:42 -0800

    Create looppattarn3.java

---
+++ looppattarn3.java
@@ -0,0 +1,19 @@
+// loop 
+// *
+// **
+// *** 
+// ****
+// *****
+
+import java.util.*;
+
+public class looppattarn3 {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
