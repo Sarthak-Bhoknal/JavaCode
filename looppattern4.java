63b145ac50691200186298612e1a1b677ec73df5
Sarthak-Bhoknal <sarthakbhoknal05@gmail.com>
2025-02-22 09:02:14 -0800

    Create looppattern4.java

---
+++ looppattern4.java
@@ -0,0 +1,19 @@
+// loop
+// ***** 
+// ****
+// ***
+// **
+// *
+
+import java.util.*;
+
+public class looppattern4 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            for (int j = i; j <= 5; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
